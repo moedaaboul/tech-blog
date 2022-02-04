368e25bdb6838a91d2816869abc2c08341c4da73
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-02-04 09:35:22 +0000

    enables register page view

---
+++ public/js/register.js
@@ -0,0 +1,42 @@
+import { makeRequest } from './helpers.js';
+
+const registerForm = document.querySelector('#register-form');
+
+const handleRegister = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#username').value;
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  const confirmPassword = document.querySelector('#confirm-password').value;
+  console.log(name);
+
+  if (
+    email &&
+    name &&
+    password &&
+    confirmPassword &&
+    password === confirmPassword
+  ) {
+    try {
+      const data = await makeRequest('/api/travellers/register', 'POST', {
+        name,
+        email,
+        password,
+      });
+      console.log(data, 'data');
+      if (data.success) {
+        window.location.replace('/login');
+        registerForm.removeEventListener('submit', handleRegister);
+      } else {
+        console.log('Failed to login');
+      }
+    } catch (error) {
+      console.log('Failed to login', error);
+    }
+  } else {
+    console.log('Failed to login');
+  }
+};
+
+registerForm.addEventListener('submit', handleRegister);
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.use('/register', (req, res) => {
+  res.render('register', { title: 'register' });
+});
+
 router.use('/login', (req, res) => {
   res.render('login', { title: 'login' });
 });
