621fffd108b34f1e78e73b6a7ae4111ed0524083
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-02-08 23:38:16 +0000

    enable markdown to be rendered in body

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,9 @@
+const { marked } = require('marked');
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
 module.exports = {
   // the helper method 'format_time' will take in a timestamp and return a string with only the time
   format_time: (date) => {
@@ -8,5 +14,8 @@ module.exports = {
       year: 'numeric',
     }).format(new Date(date));
     // return date.toLocaleTimeString();
+  },
+  format_markdown: (text) => {
+    return DOMPurify.sanitize(marked(text), { ADD_ATTR: ['target'] });
   },
 };
