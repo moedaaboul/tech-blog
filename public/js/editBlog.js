95302326ec8cd4b0107c9b3132318db1d0db3eae
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-02-14 22:51:48 +0000

    adds favicon and enables blog description edit

--- public/js/editBlog.js
+++ public/js/editBlog.js
@@ -8,14 +8,16 @@ const handleEditBlog = async (event) => {
   const blogID = document.querySelector('.title-label').id;
   const title = document.querySelector('#title').value;
   const content = document.querySelector('#content').value;
+  const description = document.querySelector('#description').value;
   if (title && content) {
     try {
       const data = await makeRequest(`/api/blogs/${blogID}`, 'PUT', {
         blog_title: title,
         blog_body: content,
+        blog_description: description,
       });
       if (data.success) {
-        window.location.replace(`/blogs/${blogID}`);
+        window.location.replace(`/`);
       } else {
         console.log('Failed to update content');
       }
